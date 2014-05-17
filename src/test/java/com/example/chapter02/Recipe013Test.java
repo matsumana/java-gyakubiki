51e8ccf1261cfc109f0b0193094d87ab868d7731
Manabu Matsuzaki <matsumana@gmail.com>
2014-05-17 17:10:35 +0900

    第2章 レシピ013 Javaのリテラルについて知りたい

---
+++ src/test/java/com/example/chapter02/Recipe013Test.java
@@ -0,0 +1,14 @@
+package com.example.chapter02;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class Recipe013Test {
+    @Test
+    public void 数値リテラル() throws Exception {
+        // Java7以降では数値リテラルをアンダーバーで区切る事が出来る
+        assertThat(10000000, is(10_000_000));
+    }
+}

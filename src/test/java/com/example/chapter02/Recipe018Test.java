8172bace37ae1ad764d126748c3ce448830b4883
Manabu Matsuzaki <matsumana@gmail.com>
2014-05-17 17:29:20 +0900

    第2章 レシピ018 &&、||と　&、|の違いを知りたい

---
+++ src/test/java/com/example/chapter02/Recipe018Test.java
@@ -0,0 +1,28 @@
+package com.example.chapter02;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class Recipe018Test {
+
+    @Test(expected = NullPointerException.class)
+    public void 論理演算子1() throws Exception {
+        // 文字列がnullだった場合にもvalue.length() != 0が実行されてしまうため、NullPointerExceptionが発生してしまう。
+        // ||と|も同様
+        String value = null;
+        if (value != null & value.length() != 0) {
+            fail();
+        }
+    }
+
+    @Test
+    public void 論理演算子2() throws Exception {
+        // 文字列がnullだった場合にはvalue.length() != 0は実行されないので、NullPointerExceptionは発生しない。
+        // ||と|も同様
+        String value = null;
+        if (value != null && value.length() != 0) {
+            System.out.println("hoge");
+        }
+    }
+}

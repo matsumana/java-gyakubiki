96e59b6250c0ef6bf46af4f2bd046aa73eaa85d5
Manabu Matsuzaki <matsumana@gmail.com>
2014-05-19 08:50:42 +0900

    whileループのサンプルコード追加

--- src/test/java/com/example/basic/chapter02/Recipe030Test.java
+++ src/test/java/com/example/basic/chapter02/Recipe030Test.java
@@ -2,7 +2,24 @@ package com.example.basic.chapter02;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class Recipe030Test {
+
+    @Test
+    public void 条件がtrueの間繰り返しを行う() throws Exception {
+
+        int i = 1;
+        int sum = 0;
+
+        while (i <= 10) {
+            sum += i;
+            i++;
+        }
+
+        assertThat(sum, is(55));
+    }
 
     @Test
     public void 最低1回は処理する時はdo_whileを使う() throws Exception {

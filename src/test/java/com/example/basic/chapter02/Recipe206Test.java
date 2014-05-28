a2aed3a3601598f4ffc5930fc077f643a5828a46
Manabu Matsuzaki <matsumana@gmail.com>
2014-05-29 03:02:02 +0900

    第2章 レシピ206 ファイルの内容を文字列で読み込みたい

---
+++ src/test/java/com/example/basic/chapter02/Recipe206Test.java
@@ -0,0 +1,33 @@
+package com.example.basic.chapter02;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class Recipe206Test {
+
+    @Test
+    public void ファイルの内容を文字列で読み込みたい() throws Exception {
+
+        // カレントディレクトリにあるファイルを読む
+        Path path = FileSystems.getDefault().getPath(
+                getClass().getResource("").getFile(),
+                "test.txt");
+
+        // InputStreamReaderを使用し、明示的にエンコーディングを指定する
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(path.toFile()), UTF_8))) {
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+    }
+}
